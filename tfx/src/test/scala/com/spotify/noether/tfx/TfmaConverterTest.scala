27d1d2a5a1073a86934de5f4834689d5c7aee801
Fallon Chen <fallon@spotify.com>
2018-08-20 16:18:10 -0400

    Add BinaryConfusionMatrix, ConfusionMatrix, ErrorRateSummary TFMA adapters

--- tfx/src/test/scala/com/spotify/noether/tfx/TfmaConverterTest.scala
+++ tfx/src/test/scala/com/spotify/noether/tfx/TfmaConverterTest.scala
@@ -17,14 +17,84 @@
 
 package com.spotify.noether.tfx
 
-import com.spotify.noether.{AUC, BinaryConfusionMatrix, ROC}
+import com.spotify.noether._
+import org.scalactic.TolerantNumerics
 import org.scalatest.{FlatSpec, Matchers}
+import tensorflow_model_analysis.MetricsForSliceOuterClass.MetricsForSlice
 
 class TfmaConverterTest extends FlatSpec with Matchers {
 
   "Stuff" should "work" in {
     val binAgg = BinaryConfusionMatrix().asTfmaProto
+
     val aucAgg = AUC(ROC).asTfmaProto
   }
 
+  "ConfusionMatrix converter" should "work" in {
+    val data = List(
+      (0, 0),
+      (0, 1),
+      (0, 0),
+      (1, 0),
+      (1, 1),
+      (1, 1),
+      (1, 1)
+    ).map { case (s, pred) => Prediction(pred, s) }
+
+    val cmProto: MetricsForSlice = ConfusionMatrix(Seq(0, 1)).asTfmaProto(data)
+
+    val cm = cmProto
+      .getMetricsMap()
+      .get("Noether_ConfusionMatrix")
+      .getConfusionMatrixAtThresholds
+      .getMatrices(0)
+
+    assert(cm.getFalseNegatives === 1L)
+    assert(cm.getFalsePositives === 1L)
+    assert(cm.getTrueNegatives === 2L)
+    assert(cm.getTruePositives === 3L)
+  }
+
+  "BinaryConfusionMatrix converter" should "work" in {
+    val data = List(
+      (false, 0.1),
+      (false, 0.6),
+      (false, 0.2),
+      (true, 0.2),
+      (true, 0.8),
+      (true, 0.7),
+      (true, 0.6)
+    ).map { case (pred, s) => Prediction(pred, s) }
+
+    val cmProto: MetricsForSlice = BinaryConfusionMatrix().asTfmaProto(data)
+
+    val cm = cmProto
+      .getMetricsMap()
+      .get("Noether_ConfusionMatrix")
+      .getConfusionMatrixAtThresholds
+      .getMatrices(0)
+
+    assert(cm.getThreshold === 0.5)
+    assert(cm.getTruePositives === 3L)
+    assert(cm.getFalseNegatives === 1L)
+    assert(cm.getFalsePositives === 1L)
+    assert(cm.getTrueNegatives === 2L)
+  }
+
+  "Error rate summary" should "work" in {
+    implicit val doubleEq = TolerantNumerics.tolerantDoubleEquality(0.1)
+    val classes = 10
+    def s(idx: Int): List[Double] = 0.until(classes).map(i => if (i == idx) 1.0 else 0.0).toList
+
+    val data =
+      List((s(1), 1), (s(3), 1), (s(5), 5), (s(2), 3), (s(0), 0), (s(8), 1)).map {
+        case (scores, label) => Prediction(label, scores)
+      }
+
+    val ersProto: MetricsForSlice = ErrorRateSummary.asTfmaProto(data)
+
+    val ersV =
+      ersProto.getMetricsMap.get("Noether_ErrorRateSummary").getDoubleValue.getValue
+    assert(ersV === 0.5)
+  }
 }

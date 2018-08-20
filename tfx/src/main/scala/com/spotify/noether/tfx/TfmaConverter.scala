88f9690fc6536122a4988dbb8138f29bc6adcfda
Andrew Martin <andrewsmartin.mg@gmail.com>
2018-08-20 15:43:13 -0400

    add package object and refactor tfx package (#27)

--- tfx/src/main/scala/com/spotify/noether/tfx/TfmaConverter.scala
+++ tfx/src/main/scala/com/spotify/noether/tfx/TfmaConverter.scala
@@ -20,12 +20,8 @@ package com.spotify.noether.tfx
 import com.google.protobuf.DoubleValue
 import com.spotify.noether._
 import com.twitter.algebird.Aggregator
-import tensorflow_model_analysis.MetricsForSliceOuterClass.ConfusionMatrixAtThresholds.ConfusionMatrixAtThreshold
+import tensorflow_model_analysis.MetricsForSliceOuterClass.ConfusionMatrixAtThresholds._
 import tensorflow_model_analysis.MetricsForSliceOuterClass._
-
-import scala.language.implicitConversions
-
-//scalastyle:off
 
 trait TfmaConverter[A, B, T <: Aggregator[A, B, _]] {
   def convertToTfmaProto(underlying: T): Aggregator[A, B, MetricsForSlice]
@@ -34,70 +30,53 @@ trait TfmaConverter[A, B, T <: Aggregator[A, B, _]] {
 object TfmaConverter {
 
   implicit val binaryConfusionMatrixConverter
-  : TfmaConverter[Prediction[Boolean, Double], Map[(Int, Int), Long], BinaryConfusionMatrix] =
-    (underlying: BinaryConfusionMatrix) => underlying
-      .andThenPresent {
-        matrix =>
-          MetricsForSlice.newBuilder()
-            .setSliceKey(SliceKey.getDefaultInstance)
-            .putMetrics("Noether_BinaryConfusionMatrix",
-              MetricValue.newBuilder()
-                .setConfusionMatrixAtThresholds(ConfusionMatrixAtThresholds.newBuilder()
-                  .setMatrices(0, ConfusionMatrixAtThreshold.newBuilder()
-                    .setThreshold(underlying.threshold)
-                    //                  .setFalseNegatives(matrix)))
-                    .build())
-                  .build())
-                .build()).build()
-      }
-
-  implicit val aucConverter: TfmaConverter[Prediction[Boolean, Double], MetricCurve, AUC] =
-    (underlying: AUC) => underlying.andThenPresent { areaValue =>
-      val metricName = underlying.metric match {
-        case ROC => "Noether_AUC:ROC"
-        case PR => "Noether_AUC:PR"
-      }
-      MetricsForSlice.newBuilder()
-        .setSliceKey(SliceKey.getDefaultInstance)
-        .putMetrics(metricName,
-          MetricValue.newBuilder()
-            .setDoubleValue(DoubleValue.newBuilder().setValue(areaValue))
-            .build())
-        .build()
+    : TfmaConverter[BinaryPred, Map[(Int, Int), Long], BinaryConfusionMatrix] =
+    new TfmaConverter[BinaryPred, Map[(Int, Int), Long], BinaryConfusionMatrix] {
+      override def convertToTfmaProto(underlying: BinaryConfusionMatrix)
+        : Aggregator[BinaryPred, Map[(Int, Int), Long], MetricsForSlice] =
+        underlying
+          .andThenPresent { matrix =>
+            MetricsForSlice
+              .newBuilder()
+              .setSliceKey(SliceKey.getDefaultInstance)
+              .putMetrics(
+                "Noether_BinaryConfusionMatrix",
+                MetricValue
+                  .newBuilder()
+                  .setConfusionMatrixAtThresholds(
+                    newBuilder()
+                      .setMatrices(0,
+                                   ConfusionMatrixAtThreshold
+                                     .newBuilder()
+                                     .setThreshold(underlying.threshold)
+                                     //                  .setFalseNegatives(matrix)))
+                                     .build())
+                      .build())
+                  .build()
+              )
+              .build()
+          }
     }
 
-}
-
-trait TfmaConversionOps[A, B, T <: Aggregator[A, B, _]] {
-  val self: T
-  val converter: TfmaConverter[A, B, T]
-  def asTfmaProto: Aggregator[A, B, MetricsForSlice]  = converter.convertToTfmaProto(self)
-}
-
-object TfmaConversionOps {
-
-  import TfmaConverter._
-
-  def apply[A, B, T <: Aggregator[A, B, _]](instance: T, tfmaConverter: TfmaConverter[A, B, T])
-  : TfmaConversionOps[A, B, T] =
-    new TfmaConversionOps[A, B, T] {
-      override val self: T = instance
-      override val converter: TfmaConverter[A, B, T] = tfmaConverter
+  implicit val aucConverter: TfmaConverter[BinaryPred, MetricCurve, AUC] =
+    new TfmaConverter[BinaryPred, MetricCurve, AUC] {
+      override def convertToTfmaProto(
+        underlying: AUC): Aggregator[BinaryPred, MetricCurve, MetricsForSlice] =
+        underlying
+          .andThenPresent { areaValue =>
+            val metricName = underlying.metric match {
+              case ROC => "Noether_AUC:ROC"
+              case PR  => "Noether_AUC:PR"
+            }
+            MetricsForSlice
+              .newBuilder()
+              .setSliceKey(SliceKey.getDefaultInstance)
+              .putMetrics(metricName,
+                          MetricValue
+                            .newBuilder()
+                            .setDoubleValue(DoubleValue.newBuilder().setValue(areaValue))
+                            .build())
+              .build()
+          }
     }
-
-  implicit def binaryConfusionMatrixConversion
-  (agg: BinaryConfusionMatrix)
-  (implicit c: TfmaConverter[Prediction[Boolean, Double],
-                             Map[(Int, Int), Long],
-                             BinaryConfusionMatrix])
-  : TfmaConversionOps[Prediction[Boolean, Double], Map[(Int, Int), Long], BinaryConfusionMatrix] =
-    TfmaConversionOps[Prediction[Boolean, Double], Map[(Int, Int), Long], BinaryConfusionMatrix](agg, c)
-
-  implicit def aucConversion(agg: AUC)
-                             (implicit c: TfmaConverter[Prediction[Boolean, Double],
-                                                        MetricCurve,
-                                                        AUC])
-  : TfmaConversionOps[Prediction[Boolean, Double], MetricCurve, AUC] =
-    TfmaConversionOps[Prediction[Boolean, Double], MetricCurve, AUC](agg, c)
 }
-//scalastyle:on

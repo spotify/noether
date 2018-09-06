e0a58554ccfdc4fcb74dd67b7c6e2a3ff917fff9
César Collé <cesar.colle@gmail.com>
2018-09-06 13:22:33 +0200

    Add spotify licence

--- benchmark/src/main/scala/com/spotify/noether/benchmark/CalibrationHistogramCreateBenchmark.scala
+++ benchmark/src/main/scala/com/spotify/noether/benchmark/CalibrationHistogramCreateBenchmark.scala
@@ -1,16 +1,33 @@
+/*
+ * Copyright 2018 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package com.spotify.noether
 package benchmark
 
 import com.spotify.noether.benchmark.CalibrationHistogramCreateBenchmark.CalibrationHistogramState
+import org.openjdk.jmh.annotations._
 
 import scala.util.Random
-import org.openjdk.jmh.annotations._
 
 
 object PredictionUtils {
 
-  def generatePredictions(nbPrediction : Int): Seq[Prediction[Boolean, Double]] ={
-    Seq.fill(nbPrediction)(Prediction(Random.nextBoolean(), Random.nextInt(99).toFloat/100))
+  def generatePredictions(nbPrediction: Int): Seq[Prediction[Boolean, Double]] = {
+    Seq.fill(nbPrediction)(Prediction(Random.nextBoolean(), Random.nextInt(99).toFloat / 100))
   }
 }
 
@@ -31,13 +48,14 @@ object CalibrationHistogramCreateBenchmark {
     @Param(Array("0.2", "0.4", "0.5"))
     var upperBound = 0.0
 
-    var histogram : CalibrationHistogram = _
+    var histogram: CalibrationHistogram = _
 
     @Setup
     def setup(): Unit = {
       histogram = CalibrationHistogram(lowerBound, upperBound, nbBucket)
     }
   }
+
 }
 
 class CalibrationHistogramCreateBenchmark {
